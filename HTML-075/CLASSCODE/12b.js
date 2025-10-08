c78cfc52e35fa7108485ce4879a8da758951260c
King-Bamzy <rahmonayobami45@gmail.com>
2025-10-08 14:53:52 +0100

    Add multiple HTML files and assets for W3School project
    - Created various HTML files demonstrating different HTML elements and attributes including links, images, lists, and styles.
    - Added CSS styles for layout and formatting.
    - Included images and favicon for enhanced visual representation.
    - Implemented responsive design principles in layout.html.
    - Introduced examples of inline, internal, and external CSS usage.
    - Added comments and structured content for better understanding of HTML structure and usage.

---
+++ HTML-075/CLASSCODE/12b.js
@@ -0,0 +1,7 @@
+function add()
+{
+    var num1 = parseInt(prompt("Enter first number for addition:"));
+    var num2 = parseInt(prompt("Enter second number for addition:"));
+    var sum = num1 + num2;
+    alert("Addition: " + sum);
+}
---
+++ HTML-075/CLASSCODE/session12.js
@@ -0,0 +1,42 @@
+const data = {
+    "page": 1,
+    "results": [
+        {
+            "first_air_date": "2005-03-26",
+            "genre_ids": [
+                28,
+                12,
+                18,
+                878
+            ],
+            "id": 57243,
+            "original_name": "Doctor Who",
+            "origin_country": ["GB"],
+            "name": "Doctor Who"
+        },
+        {
+            "first_air_date": "2007-09-24",
+            "genre_ids": [
+                18,
+                35
+            ],
+            "id": 1418,
+            "original_name": "The Big Bang Theory",
+            "origin_country": ["US"],
+            "name": "The Big Bang Theory"
+        },
+        {
+            "first_air_date": "2015-08-23",
+            "genre_ids": [
+                18,
+                27
+            ],
+            "id": 62286,
+            "original_name": "Fear the Walking Dead",
+            "origin_country": ["US"],
+            "name": "Fear the Walking Dead"
+        }
+    ],
+    "total_pages": 3116,
+    "total_results": 62309
+};
